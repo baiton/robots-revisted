9df60c579958856ffc65937cc08f3b75b13497d8
Brandon Aiton <brandonaiton@icloud.com>
2017-08-28 08:50:13 -0400

    I still cannot get my promise to work so I see no robots

---
+++ dal.js
@@ -0,0 +1,163 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/robots';
+let robots = [];
+
+function getAllDocs(err, db){
+  console.log(err);
+  const collection = db.collection('users')
+  let documents = [];
+  collection.find({}).toArray(function(err, docs){
+    robots = docs;
+    db.close
+  })
+}
+
+function getAllRobots (){
+  return new Promise ((resolve, reject) =>{
+    MongoClient.connect(url, function(err, db){
+      // console.log(db);
+      const collection = db.collection('users')
+      collection.find({}).toArray(function(err, docs){
+        // console.log("docs", docs);
+        resolve(docs);
+        reject(err);
+      })
+    })
+  })
+}
+
+function connectToMongodb(url, cb){
+  MongoClient.connect(url, cb)
+}
+
+function getRobots(){
+  connectToMongodb(url, getAllDocs())
+    return robots;
+  }
+
+  module.exports = {
+    getAllRobots: getAllRobots,
+    getRobots: getRobots
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // now what do i need?  make an array for each group of robots you will need
+  // let bots by country = [] //hard mode
+
+
+  // first you have to connect to the server where your db is
+
+
+// MongoClient.connect(url)
+//   .then(function (db) { // <- db as first argument
+//     console.log("db", db)
+//   })
+//   .catch(function (err) {})
+//
+//
+// function getAllDocs(err, db) {
+//   const collection = db.collection('people')
+//   let documents = [];
+//   collection.find({}).toArray(function(err, docs) {
+//     bots = docs;
+//     db.close()
+//   })
+// }
+//
+//
+// async function getAllRobots() {
+//   var db = await MongoClient.connect(url);
+//   let collection = db.collection('users');
+//   // for each robot object/document in users collection, assign to documents which assigns robots
+//   let documents = [];
+//   var robots = await collection.find();
+//   return robots;
+//   console.log("robots", robots);
+// }
+// get only 1 robot
+// function getRobot(robotId) {
+//   let chosenRobot = {}
+//   for (i = 0; i < robot.length; i++) { //look into the for loop like "for (let i in robots){ }"
+//     if (robot[i].id === robotId) {
+//       chosenRobot = robot[i];
+//     }
+//   }
+//   return chosenRobot;
+// }
+//
+// // unemployed robots only
+// async function findUnemployedRobots(err, db) {
+//   // console.log('error 1' + err);
+//   let collection = db.collection('robots');
+//   let documents = [];
+//   let unemployedRobots = await collection.find({
+//     job: null
+//   })
+//   return unemployedRobots;
+// };
+//
+// function getUnemployedRobots() {
+//   connectToMongodb(url, findUnemployedRobots)
+//   console.log(unemployedRobots);
+//   return unemployedRobots;
+// }
+//
+// // employed robots only
+// function findEmployedRobots(err, db) {
+//   // console.log('error 1' + err);
+//   let collection = db.collection('robots');
+//   let documents = [];
+//   collection.find({
+//     jobs: {
+//       $not: {
+//         $in: [null]
+//       }
+//     }
+//   }).toArray(function(err, docs) {
+//     employedRobots = docs;
+//     db.close();
+//   });
+// }
+//
+// function getEmployedRobots() {
+//   connectToMongodb(url, findEmployedRobots)
+//   console.log(employedRobots);
+//   return employedRobots;
+// }
+//
+
+
+
+// old code
+// function getRobots(){
+//   return robot;
+// }
+//
+//
+// function getRobot(robotId){
+// let chosenRobot={}
+// for (i=0;i < robot.length ; i++){
+//   if (robot[i].id === robotId){
+//     chosenRobot = robot[i];
+//   }
+// }
+// return chosenRobot;
+// }
+//
+// module.exports = {
+//   getRobots: getRobots,
+//   getRobot: getRobot,
+// }
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+// const robotsData = require('./data');
+const robotDal = require('./dal');
+const bodyParser = require('body-parser');
+// const robot= robotsData.users;
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+// console.log(robotsData.users)
+
+// all bots
+app.get('/', function(request, response){
+    const bots = robotDal.getRobots();
+    console.log(bots);
+  response.render('robots', {
+    robots: bots
+  })
+})
+//
+// // 1 bot profile
+// app.get('/robots/:id', function (request, response) {
+//   const chosenRobot = robotDal.getRobot(parseInt(request.params.id));
+//   if (chosenRobot.id) {
+//     response.render('robotDetails', chosenRobot);
+//   } else {
+//     response.send('NO ROBOT!!!');
+//   }
+// })
+//
+// app.get('/employedbots', function (request, response){
+//   const roboEmp = robotDal.getEmployedRobots();
+//   response.render('employed', {roboEmp});
+// })
+//
+// app.get('/unemployedbots', function (request, response){
+//   const roboUnEmp = robotDal.getUnemployedRobots();
+//   response.render('unemployed', {roboUnEmp});
+// })
+
+
+app.set ('port', 3000);
+
+app.listen (3000, function(){
+  console.log('Application has started at Port 3000');
+});
